28fc3bf9a815b3d1c7fbf21e252a0f80f0d6c76f
1994dsp <167685416+1994dsp@users.noreply.github.com>
2024-11-13 23:10:49 +0000

    changes to blueprintSpec.ts

--- test/server/blueprintSpec.ts
+++ test/server/blueprintSpec.ts
@@ -43,8 +43,12 @@ describe('blueprint', () => {
           } else {
             pathToImage = path.resolve('frontend/src', pathToImage, product.image)
           }
-
-          if (product.exifForBlueprintChallenge?.[0]) { // Prevents failing test for sample or custom themes where null has been explicitly set as value for "exifForBlueprintChallenge". Warning: This makes the "Retrieve Blueprint" challenge probably unsolvable unless hints are placed elsewhere.
+          console.log(`Testing EXIF data for image at path: ${pathToImage}`);
+          if (!product.exifForBlueprintChallenge?.[0]) {
+            console.warn(`Skipping EXIF test for ${pathToImage} as no EXIF properties are defined.`);
+            continue; // Skip if no EXIF properties are required
+          } 
+             // Prevents failing test for sample or custom themes where null has been explicitly set as value for "exifForBlueprintChallenge". Warning: This makes the "Retrieve Blueprint" challenge probably unsolvable unless hints are placed elsewhere.
             try {
               const exifData = await parseExifData(pathToImage)
               const properties = Object.values(exifData.image)
@@ -54,7 +58,7 @@ describe('blueprint', () => {
             } catch (error) {
               expect.fail(`Could not read EXIF data from ${pathToImage}`)
             }
-          }
+          
         }
       }
     })
